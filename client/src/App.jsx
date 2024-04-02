611a63e9b9a98384be945f2d9b7ac5cf183ed6fc
garrettperson <gagmac@gmail.com>
2024-04-01 21:34:33 -0700

    changed main.jsx, app.jsx, got home page to display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
-
   return (
     <>
-      <h1>Hello world!</h1>
+      <div className="logo">
+        <Outlet />
+      </div>
     </>
-  )
-};  
+  );
+}
 
 export default App;
