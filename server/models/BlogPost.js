fcd001c454c6dcb0309908dac18208160d1297dd
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2024-04-01 21:28:29 -0400

    quickly added models for users and blogposts
    also added util folder with auth.js and dateFormat.js files

---
+++ server/models/BlogPost.js
@@ -0,0 +1,49 @@
+const { Schema } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
+
+const blogPostSchema = new Schema({
+    blogTitle: {
+        type: String,
+        required: 'Title cannot be empty!',
+        minlength: 1,
+        maxlength: 280,
+        trim: true,
+    },
+    blogText: {
+        type: String,
+        required: 'blog area cannot be empty!',
+        trim: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    comments: [
+        {
+            commentText: {
+                type: String,
+                required: true,
+                minlength: 1,
+                maxlength: 300,
+            },
+            commentAuthor: {
+                type: String,
+                required: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now,
+                get: (timestamp) => dateFormat(timestamp),
+              },
+        },
+    ],
+});
+
+const BlogPost = model('BlogPost', blogPostSchema);
+module.exports = BlogPost;
