40d5bc76bf63ade1cb3f096dddf753ace7881b38
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2024-04-03 23:54:13 -0400

    added babel and styled-components packages to client package.json
    so the styled components package will work properly

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -7,9 +7,26 @@ import { ADD_USER } from '../utils/mutations'
 import Auth from '../utils/auth';
 import styled from 'styled-components'
 
+
+// Styled Components
+const Form = styled.form`
+        background-color: #F7F6FE;
+        height: 800px;
+        width: 900px;
+        padding: 20px;
+        margin: 20px;
+        border: solid 5px #455A30;
+        `
+const Input = styled.input`
+        height: 30px;
+        width: 350px;
+        background-color: white;
+        border: solid 3px #0C1117;
+        font-family: monospace,
+        font-size: 12px;
+        color: #01050A;`
+
 const SignUp = () => {
-
-
     const [formState, setFormState] = useState({
         firstName: '',
         lastName: '',
@@ -45,27 +62,7 @@ const SignUp = () => {
         }
     };
 
-
-    // eslint-disable-next-line no-unused-vars
-    // Note from Leesa: I have no idea why it's reading as not being used when it CLEARLY is
-
-    const form = styled.form`
-        background-color: #F7F6FE;
-        height: 800px;
-        width: 900px;
-        padding: 20px;
-        margin: 20px;
-        border: solid 5px #455A30;
-        `
-    const inputFirstName = styled.form`
-        height: 30px;
-        width: 350px;
-        background-color: white;
-        border: solid 3px #0C1117;
-        font-family: monospace,
-        font-size: 12px;
-        color: #01050A;`
-
+    
     return (
         <main>
             <div>
@@ -76,48 +73,48 @@ const SignUp = () => {
                             <p>Success! You may now head{' '}
                                 <Link to="/">back to the homepage.</Link></p>
                         ) : (
-                            <form onSubmit={handleFormSubmit}>
-                                <input
+                            <Form onSubmit={handleFormSubmit}>
+                                <Input
                                     className="form-input"
                                     placeholder="first name"
                                     name="first name"
                                     type="text"
                                     value={formState.firstName}
-                                    onChange={handleChange}></input>
-                                <input
+                                    onChange={handleChange}></Input>
+                                <Input
                                     className="form-input"
                                     placeholder="last name"
                                     name="last name"
                                     type="text"
                                     value={formState.lastName}
-                                    onChange={handleChange}></input>
-                                <input
+                                    onChange={handleChange}></Input>
+                                <Input
                                     className="form-input"
                                     placeholder="Username"
                                     name="username"
                                     type="text"
                                     value={formState.username}
-                                    onChange={handleChange}></input>
-                                <input
+                                    onChange={handleChange}></Input>
+                                <Input
                                     className="form-input"
                                     placeholder="email"
                                     name="email"
                                     type="text"
                                     value={formState.email}
-                                    onChange={handleChange}></input>
-                                <input
+                                    onChange={handleChange}></Input>
+                                <Input
                                     className="form-input"
                                     placeholder="********"
                                     name="password"
                                     type="password"
                                     value={formState.password}
-                                    onChange={handleChange}></input>
+                                    onChange={handleChange}></Input>
                                 <button
                                     style={{ cursor: 'pointer' }}
                                     type="submit">
                                     Submit
                                 </button>
-                            </form>
+                            </Form>
                         )}
 
                         {error && (
