9021e155c00903122d2d746dde6204a6de3271c6
garrettperson <gagmac@gmail.com>
2024-04-08 13:34:13 -0700

    add comment visible on forum page

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -6,12 +6,12 @@ export default function Nav() {
         <Navbar 
         links={[
             <Link key={1} to="/">Home</Link>,
-            <Link key={2} to="/login"></Link>,
-            <Link key={3} to="/signup"></Link>,
-            <Link key={4} to="/facts"></Link>,
-            <Link key={5} to="/forum"></Link>,
-            <Link key={6} to="/account"></Link>,
-            <Link key={7} to="/deleteaccount"></Link>
+            <Link key={2} to="/login">Login</Link>,
+            <Link key={3} to="/signup">Sign Up</Link>,
+            <Link key={4} to="/facts">Facts</Link>,
+            <Link key={5} to="/forum">Forum</Link>,
+            <Link key={6} to="/account">Account</Link>,
+            <Link key={7} to="/deleteaccount">Delete Account</Link>
         ]}></Navbar>
     )
 }
